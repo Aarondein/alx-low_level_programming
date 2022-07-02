b0faf10155fd78641bcacdb23a45c57e073e8679
Aarondein <aarondein50@gmail.com>
2022-07-02 03:48:05 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
-/** 
-* main - Entry point
-* 
-* Return: Always 0(Success) 
-*/
+
+#include <string.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Description: The programmer's life quote
+ *
+ *    * Return: Always 0 (success)
+ *
+ *    */
+
 int main(void)
-{    
-puts("\"Programming is like building a multilingual puzzle");    
-return (0);
+
+{
+
+		char str1[52];
+
+
+
+			strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+
+
+				puts(str1);
+
+
+
+					return (0);
+
 }
